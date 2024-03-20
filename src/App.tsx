080323c386a4e01ee1dba9c53c2ae91288c84584
Aidar Ibraev <aidaribraev32@gmail.com>
2024-03-20 18:51:54 +0600

    Commit - 1.1.2. Created new components - AccordionTitle and AccordionBody. Added console.log to see steps of rendering

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    console.log("App rendering")
     return (
         <div>
-            Hello World, I'm React App)
+            <AppTitle />
             <Rating />
             <Accordion />
+            <Rating />
         </div>
     );
 }
 
+function AppTitle()  {
+    console.log("AppTitle rendering")
+    return <>Hello World, I'm React App)</>
+}
+
 function Rating() {
+    console.log("Rating rendering")
     return (
         <div>
             <Star />
@@ -24,22 +32,36 @@ function Rating() {
 }
 
 function Star() {
+    console.log("Star rendering")
     return (
         <div>star</div>
     )
 }
 
 function Accordion() {
+    console.log("Accordion rendering")
     return (
         <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-            </ul>
+            <AccordionTitle />
+            <AccordionBody />
         </div>
+    )
+}
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return <h3>Menu</h3>
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
     )
 }
 
