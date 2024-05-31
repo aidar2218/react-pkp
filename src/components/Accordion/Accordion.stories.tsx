368c84b6cd4c034da6dd828eef6ca8a1c63f536c
Aidar Ibraev <aidaribraev32@gmail.com>
2024-05-31 23:35:44 +0600

    Commit - 2.1.10. Storybook ch.1. Introduce

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import {Accordion} from './Accordion';
+import {useState} from "react";
+
+const meta: Meta<typeof Accordion> = {
+    component: Accordion,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Accordion>;
+
+const onChangeHandler = action("onChange was clicked")
+
+export const FirstTestAccordion: Story = {
+    args: {
+        titleValue: "Simple Accordion",
+        collapsed: false,
+        onChange: onChangeHandler
+    }
+}
+
+
+
+export const CollapsedAccordion = () => {
+    return (
+        <Accordion titleValue={"Collapse"}
+                   collapsed={true}
+                   onChange={onChangeHandler}
+        />
+    )
+}
+
+export const OpenedAccordion = () => {
+    return (
+        <Accordion titleValue={"Open"}
+                   collapsed={false}
+                   onChange={onChangeHandler}
+        />
+    )
+}
+
+export const DemoAccordion = () => {
+
+    const [collapsed, setCollapsed] = useState(false)
+
+    return (
+        <Accordion titleValue={"Demo"}
+                   collapsed={collapsed}
+                   onChange={() => {setCollapsed(!collapsed)}}
+        />
+    )
+}
